c33ea0407d462d2a1ee79e6bd9e7054d005b5d63
Matt Borek <z1951125@students.niu.edu>
2022-11-24 05:46:25 -0600

    Separate and format dynamic draw functions

--- lib/drinksLib.php
+++ lib/drinksLib.php
@@ -101,4 +101,90 @@ function drawCards($rows)
     }
 }
 
+function drawCartItem(PDO &$pdo, $item, $quantity)
+{
+    $sql = <<<SQL
+    SELECT * FROM DRINKS
+    WHERE NAME = ?
+    SQL;
+
+    try
+    {   
+        $statement = $pdo->prepare($sql);
+        $statement->execute([$item]);
+        $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch(PDOException $e)
+    {
+        echo "Query Failure: " . $e->getMessage();
+    }
+
+    if(empty($rows)) { echo "<p>No results found.</p>"; }
+    else 
+    {
+        foreach($rows as $row)
+        {
+            $filename = str_replace(' ', '', $row['NAME']);
+            $urlName = str_replace(' ', '-', $row['NAME']);
+            $floatPrice = number_format((float)$row['PRICE'], 2, '.', '');
+            $adjustedPrice = number_format((float)($quantity * $floatPrice), 2, '.', '');
+
+            echo <<<HTML
+            <div class="cartItem">
+                <div class="card mb-4" id="borderCALI">
+                    <div class="card-body p-4">
+                        <div class="row d-flex justify-content-between align-items-center">
+                            <div class="col-md-2 col-lg-2 col-xl-2">
+                                <img src="assets/drinks/$filename.png">
+                            </div>
+                            <div class="col-md-3 col-lg-3 col-xl-3">
+                                <p class="fw-normal mb-2 fs-5 Adelhyde">{$row['NAME']}</p>
+                                <p class="Flanergide">
+                                    <span class="Bronson">Flavor: </span>{$row['FLAVOR']} <span class="Bronson">Type: </span>{$row['TYPE']}<br>
+                                    <span class="Bronson">Catagory: </span>{$row['CATEGORY']} <span class="Karmotrine">Price: $</span><span class="Karmotrine price">$floatPrice</span>
+                                </p>
+                            </div>
+                            <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                                <button class="btn btn-link px-2" onclick="this.parentNode.querySelector('input[type=number]').stepDown(), adjustCost(this.parentNode.querySelector('input[type=number]'))">
+                                    <i class="fas fa-minus Delta"></i>
+                                </button>
+
+                                <input id="Item" name="item" value="{$row['NAME']}" type="hidden"/>
+                                <input id="Quantity" min="0" max=$row[STOCK] name="quantity" value=$quantity type="number" class="form-control form-control-sm Quantity"/>
+            
+                                <button class="btn btn-link px-2" onclick="this.parentNode.querySelector('input[type=number]').stepUp(), adjustCost(this.parentNode.querySelector('input[type=number]'))">
+                                    <i class="fas fa-plus Delta"></i>
+                                </button>
+                            </div>
+                            <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1"><h2 class="mb-0 Karmotrine cost">$$adjustedPrice</h2></div>
+                            <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                                <button class="btn btn-link px-2" onclick="removeCart(this, '{$row['NAME']}')">
+                                    <i class="fas fa-trash fa-lg Stella"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            HTML;
+        }
+    }
+}
+
+function drawCart(PDO &$pdo)
+{
+    if(isset($_SESSION['cart']) && !empty($_SESSION['cart']))
+    {
+        foreach($_SESSION['cart'] as $item => $quantity)
+        {
+            drawCartItem($pdo, $item, $quantity);
+        }
+    }
+    else 
+    {
+        echo "<h2 class=\"px-4\">Your cart is empty.</h2>";
+    }
+}
+
 ?>
