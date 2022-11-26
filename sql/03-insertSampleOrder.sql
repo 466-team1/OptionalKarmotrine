2abad415db8e7601bb360793bd331a96d5f36bc0
Matt Borek <z1951125@students.niu.edu>
2022-11-25 20:02:09 -0600

    Add sample order insert sql

---
+++ sql/03-insertSampleOrder.sql
@@ -0,0 +1,17 @@
+INSERT INTO `ORDERS`(`COST`, `CUS_NAME`, `CUS_ADDRESS`, `CUS_EMAIL`, `STATUS`, `NOTE`, `TRACKING`) VALUES
+  (1040.00, 'Alma Armas', '404 Deltor Ave, Glitch City, Neo California', 'alma@pm.me', 'CONFIRMED', '', '95720932'),
+  (1350.00, 'Rebecca Willow', '113 Dolls Frontline, Glitch City, Neo California', 'DFC72@gnk.ru', 'PROCESSING', '', '83067286'),
+  (1600.00, 'Radigan D. Dog', '802-11 Picus Plaza, Glitch City, Neo California', 'rad5hiba@glitchcity.nc', 'SHIPPED', '', '83586315'),
+  (1600.00, 'Berezovich Kryuger', '88 Gunboat Boulevard, Heliansfield, Neo Soviet Union', 'kryuger@gnk.ru', 'SHIPPED', 'Rush job, velvet glove treatment', '98365010'),
+  (1600.00, 'Alice Rabbit', '89 DangerU Dr, Glitch City, Neo California', 'admin@dangeru.moe', 'CONFIRMED', '', '31467843'),
+  (1600.00, 'Havier Witkin', '451 IOP Street, Berlin, Germany', 'witkin@IOP.net', 'COMPLETED', '', '31467843');
+
+INSERT INTO `HAS`(`ORDER_NUM`, `NAME`, `QTY`) VALUES
+  (1, 'Brandtini', 2),
+  (2, 'Piano Woman', 3),
+  (3, 'Grizzly Temple', 1),
+  (3, 'Fluffy Dream', 1),
+  (4, 'Fringe Weaver', 2),
+  (5, 'Cobalt Velvet', 3),
+  (6, 'Piano Man', 1),
+  (6, 'Piano Woman', 2);
