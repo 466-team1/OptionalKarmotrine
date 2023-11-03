d2d4dc3bb593c706386c0dd795c7d4f2e5ef473e
Matt <20992519+Eremeir@users.noreply.github.com>
2023-11-02 21:36:08 -0500

    update tademark to trademark

--- html/orderTrack.php
+++ html/orderTrack.php
@@ -99,7 +99,7 @@ require_once '../lib/drinksLib.php';
     </div>
     <footer class="pt-2 mt-4 text-muted border-top">
       Copyright (c) Keeree Joe Group. 2064.
-      CALICOMP and Keeree Joe Group are registered tademarks of Banjo Group.
+      CALICOMP and Keeree Joe Group are registered trademarks of Banjo Group.
     </footer>
   </main>
   <!-- Bootstrap JavaScript Libraries -->
